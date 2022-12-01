358b48920e53157c76e60be0dad56c75458f30b6
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2022-12-01 10:47:59 +0500

    Chat Room Initialized

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class RoomsController < ApplicationController
+  def index
+    @current_user = current_user
+    redirect_to '/signin' unless @current_user
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room  = Room.new # For Creating New Room
+  end
+
+  def create
+    @room = Room.create(name: params['room']['name'])
+  end
+
+  def show
+    @current_user = current_user
+    @single_room  = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room  = Room.new
+
+    render 'index'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+#routes.rb
+Rails.application.routes.draw do
+  get 'rooms/index'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  resources :rooms
+  resources :users
+  root 'rooms#index'
+end
