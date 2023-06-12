3bdf3fda7ea87e248cfc1ff99f98213a803ea474
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-06-12 23:17:57 +0500

    Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
   config.assets.css_compressor = :yui
-  config.assets.js_compressor  = :terser
+  config.assets.js_compressor = :uglifier
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
