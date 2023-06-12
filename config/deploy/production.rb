1a9eed4fd61c359aac0ea5fbaa39f0416869b5da
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-06-12 17:53:35 +0500

    Gemfile Updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,6 @@
-server '16.170.219.97', user: 'deploy', roles: %w{app db web}
+set :branch, 'main'
+
+set :deploy_to, '/var/www/projects/'
+set :rails_env, 'production'
+
+server '16.170.219.97', user: 'deploy', roles: %w{app db web}, primary: true
