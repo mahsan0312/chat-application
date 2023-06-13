1400b4746c242e06af293697b439fb7600593bfb
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-06-13 14:46:06 +0500

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :branch, 'main'
 
-set :deploy_to, '/var/www/projects/'
+set :deploy_to, '/var/www/projects/current'
 set :rails_env, 'production'
 
 server '16.170.219.97', user: 'deploy', roles: %w{app db web}, primary: true
