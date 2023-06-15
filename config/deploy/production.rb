05b8b21808e44e073ea2ee636cf3523a28cbb42f
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-06-15 18:40:56 +0500

    Fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,6 @@ set :deploy_to, '/var/www/projects/current'
 set :rails_env, 'production'
 
 server '16.170.219.97', user: 'deploy', roles: %w{app db web}, primary: true
+set :passenger_environment_variables, {
+  'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/run/passenger-instreg'
+}
